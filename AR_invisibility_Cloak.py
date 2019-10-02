58c2afd11376ed43f2bb70673c6897a438547a6c
Subhaditya <msubhaditya@gmail.com>
2019-10-02 14:23:58 +0530

    Changed to a different version of the implementation. Use file range_det to easily change hsv values if you want another color instead of red.

--- AR_invisibility_Cloak.py
+++ AR_invisibility_Cloak.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import time
+import pickle
 
 print("""
 
@@ -8,11 +9,12 @@ Harry :  Hey !! Would you like to try my invisibility cloak ??
 
          Its awesome !!
 
-        
+
          Prepare to get invisible .....................
     """)
 
-
+lower_red = np.array([t[0],t[1],t[2]])
+upper_red = np.array([t[3],t[4],t[5]])
 cap = cv2.VideoCapture(0)
 time.sleep(3)
 background=0
@@ -26,20 +28,17 @@ while(cap.isOpened()):
 
 	img = np.flip(img,axis=1)
 	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	value = (35, 35)
-	blurred = cv2.GaussianBlur(hsv, value,0)
-	lower_red = np.array([0,120,70])
-	upper_red = np.array([10,255,255])
 	mask1 = cv2.inRange(hsv,lower_red,upper_red)
 
-	lower_red = np.array([170,120,70])
-	upper_red = np.array([180,255,255])
-	mask2 = cv2.inRange(hsv,lower_red,upper_red)
+	mask1 = cv2.morphologyEx(mask1,cv2.MORPH_OPEN,np.ones((3,3),np.uint8))
+	mask1 = cv2.morphologyEx(mask1,cv2.MORPH_DILATE,np.ones((3,3),np.uint8))
 
-	mask = mask1+mask2
-	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5),np.uint8))
-	
-	img[np.where(mask==255)] = background[np.where(mask==255)]
+	mask2 = cv2.bitwise_not(mask1)
+	res1 = cv2.bitwise_and(img,img,mask=mask2)
+
+	res2 = cv2.bitwise_and(background,background,mask=mask1)
+
+	final = cv2.addWeighted(res1,1,res2,1,0)
 	cv2.imshow('Display',img)
 	k = cv2.waitKey(10)
 	if k == 27:
